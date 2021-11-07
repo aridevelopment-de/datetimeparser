02c70eb553713b0006b54235a44ac91062d1e669
NekoFanatic <83883849+NekoFanatic@users.noreply.github.com>
2021-11-07 10:47:12 +0100

    Update baseclasses.py

--- datetimeparser/baseclasses.py
+++ datetimeparser/baseclasses.py
@@ -21,7 +21,7 @@ class RelativeDate:
         self.days = days
 
 class RelativeTime:
-    def __init__(self, hours=None, minutes=0, seconds=0):
+    def __init__(self, hours=0, minutes=0, seconds=0):
         self.hours = hours
         self.minutes = minutes
         self.seconds = seconds
