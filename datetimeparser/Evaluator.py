699ce732603a4927444c9ef2ea88b72db98984d1
NekoFanatic <83883849+NekoFanatic@users.noreply.github.com>
2021-11-08 21:27:55 +0100

    edited evaluator.py and added test imports

--- datetimeparser/Evaluator.py
+++ datetimeparser/Evaluator.py
@@ -9,18 +9,30 @@ class Evaluator:
 
     def evaluate(self) -> datetime:
         out = ""
-        if len(self.parsed_object) == 2:
-            for object_type in self.parsed_object:
-                if isinstance(object_type, AbsoluteDateTime):
-                    out += f"{object_type.year}-{object_type.month}-{object_type.day} "
+        if self.parsed_object[0] == Method.ABSOLUTE_DATE_FORMATS:
 
-                if isinstance(object_type, AbsoluteClockTime):
-                    out += f"{object_type.hour}:{object_type.minute}:{object_type.second}"
-        else:
-            if isinstance(self.parsed_object[0], AbsoluteDateTime):
-                out += f"{self.parsed_object[0].year}-{self.parsed_object[0].month}-{self.parsed_object[0].day} 0:00:00"
-            if isinstance(self.parsed_object[0], AbsoluteClockTime):
-                out += f"{datetime.strftime(datetime.today(), '%Y-%m-%d')} {self.parsed_object[0].hour}:{self.parsed_object[0].minute}:{self.parsed_object[0].second}"
+            if len(self.parsed_object[1]) == 2:
+                for object_type in self.parsed_object[1]:
+                    if isinstance(object_type, AbsoluteDateTime):
+                        out += f"{object_type.year}-{object_type.month}-{object_type.day} "
+
+                    if isinstance(object_type, AbsoluteClockTime):
+                        out += f"{object_type.hour}:{object_type.minute}:{object_type.second}"
+            else:
+                if isinstance(self.parsed_object[1][0], AbsoluteDateTime):
+                    out += f"{self.parsed_object[1][0].year}-{self.parsed_object[1][0].month}-{self.parsed_object[1][0].day} 0:00:00"
+                if isinstance(self.parsed_object[1][0], AbsoluteClockTime):
+                    out += f"{datetime.strftime(datetime.today(), '%Y-%m-%d')} {self.parsed_object[1][0].hour}:{self.parsed_object[1][0].minute}:{self.parsed_object[1][0].second}"
+        
+        if self.parsed_object[0] == Method.ABSOLUTE_PREPOSITIONS:
+            pass
+
+        if self.parsed_object[0] == Method.CONSTANTS:
+            pass
+
+        if self.parsed_object[0] == Method.RELATIVE_DATETIMES:
+            pass
+
         if out:
             try:
                 dt_object = datetime.strptime(out, "%Y-%m-%d %H:%M:%S")
--- tests/absolute_prepositions.py
+++ tests/absolute_prepositions.py
@@ -1,12 +1,14 @@
 def run():
     try:
         from datetimeparser.Parser import Parser
+        from datetimeparser.Evaluator import Evaluator
         from Colors import Colors
     except ImportError:
         import sys
         sys.path.insert(0, "..")
 
         from datetimeparser.Parser import Parser
+        from datetimeparser.Evaluator import Evaluator
         from Colors import Colors
 
     test_cases = [
@@ -23,8 +25,11 @@ def run():
         p = Parser(testcase)
         parser_result = p.parse()
 
+        e = Evaluator(parser_result)
+        evaluator_result = e.evaluate()
+
         print(Colors.ANSI_GREEN + "Testcase:", Colors.ANSI_CYAN + testcase + Colors.ANSI_RESET)
         print(Colors.ANSI_GREEN + "Parser:", Colors.ANSI_YELLOW + str(parser_result) + Colors.ANSI_RESET)
-        print(Colors.ANSI_GREEN + "Evaluator:", Colors.ANSI_YELLOW + str(None) + Colors.ANSI_RESET)
+        print(Colors.ANSI_GREEN + "Evaluator:", Colors.ANSI_PURPLE + str(evaluator_result) + Colors.ANSI_RESET)
         print(Colors.ANSI_BLUE + ("=" * 50))
         print()
