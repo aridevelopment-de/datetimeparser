e48a455890683b69738ab8c91313610c6be749f8
Ari24-cb24 <ari.publicmail@gmail.com>
2021-11-14 17:23:37 +0100

    Added testcases

--- tests/absolute_prepositions.py
+++ tests/absolute_prepositions.py
@@ -1,3 +1,7 @@
+import datetime
+
+today = datetime.datetime.today()
+
 tests = [
     "second day after christmas",
     "3rd week of august",
@@ -9,5 +13,11 @@ tests = [
 ]
 
 validation = [
-
+    datetime.datetime(year=today.year, month=12, day=25),
+    datetime.datetime(year=today.year, month=8, day=21),
+    datetime.datetime(year=today.year, month=8, day=28),
+    datetime.datetime(year=today.year, month=8, day=1),
+    datetime.datetime(year=2021, month=5, day=1),
+    datetime.datetime(year=today.year, month=8, day=2),
+    datetime.datetime(year=today.year, month=5, day=21)
 ]
--- tests/relative_events.py
+++ tests/relative_events.py
@@ -1,16 +1,23 @@
+import datetime
+
+today = datetime.datetime.today()
+
 tests = [
     "next christmas",
     "christmas",
     "new years eve",
     "xmas 2025",
-    "eastern 2010",
     "next friday",
     "next second",
     "last month",
     "next hour",
-    "next year"
+    "next year",
+    "eastern 2010"
 ]
 
 validation = [
-
+    datetime.datetime(today.year, 12, 25),
+    datetime.datetime(today.year, 12, 25),
+    datetime.datetime(today.year, 12, 31),
+    datetime.datetime(2025, 12, 25)
 ]
