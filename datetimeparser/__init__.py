25666c273062eec456c9c8caa854265ac069716e
Ari24-cb24 <ari.publicmail@gmail.com>
2022-02-06 18:46:14 +0100

    Changed namings of evaluator and parser to lowercase

--- datetimeparser/__init__.py
+++ datetimeparser/__init__.py
@@ -1,5 +1,5 @@
-from datetimeparser import Parser
-from datetimeparser import Evaluator
+from datetimeparser import parser
+from datetimeparser import evaluator
 from datetimeparser import baseclasses
 
 from datetimeparser.parser import Parser
