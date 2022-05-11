f445e08b749bdbaab5b63ee62948240c15719bac
NekoFanatic <83883849+NekoFanatic@users.noreply.github.com>
2022-05-11 20:18:26 +0200

    Reformatted testcases.py

--- tests/testcases.py
+++ tests/testcases.py
@@ -94,12 +94,7 @@ testcases = {
         "hour past christmas": Expected(month=12, day=25, hour=1),
         "30 minutes past easter": None,
         # GitHub issue #158
-        "30 hours after 30.03.2020": Expected(year=2020, month=3, day=31, hour=6),
-        # GitHub issue #176
-        "piday": Expected(year=2023, month=3, day=14),
-        "10 days after pi-day": Expected(year=2023, month=3, day=14, delta=relativedelta(days=10)),
-        "tauday": Expected(month=6, day=28),
-        "10 days before tau day": Expected(month=6, day=28, delta=relativedelta(days=-10))
+        "30 hours after 30.03.2020": Expected(year=2020, month=3, day=31, hour=6)
     },
     # Relative Datetimes
     "relative_datetimes": {
@@ -151,6 +146,11 @@ testcases = {
         "begin of advent of code 2022": Expected(year=2022, month=12, day=1, hour=6),
         "end of aoc 2022": Expected(year=2022, month=12, day=26, hour=6),
         "end of the year": Expected(month=12, day=31, hour=23, minute=59, second=59),
+        # GitHub issue #176
+        "piday": Expected(year=2023, month=3, day=14),
+        "10 days after pi-day": Expected(year=2023, month=3, day=14, delta=relativedelta(days=10)),
+        "tauday": Expected(month=6, day=28),
+        "10 days before tau day": Expected(month=6, day=28, delta=relativedelta(days=-10)),
     },
     # Constant Relative Extensions
     "constants_relative_expressions": {
