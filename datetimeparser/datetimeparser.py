83065c027805f082282d8f11ed6f1307945016e5
NekoFanatic <83883849+NekoFanatic@users.noreply.github.com>
2022-06-12 11:08:15 +0200

    Updated version number

--- datetimeparser/datetimeparser.py
+++ datetimeparser/datetimeparser.py
@@ -3,7 +3,7 @@ Main module which provides the parse function.
 """
 
 __all__ = ['parse', '__version__', '__author__']
-__version__ = "0.14.0"
+__version__ = "0.13.0"
 __author__ = "aridevelopment"
 
 import datetime
